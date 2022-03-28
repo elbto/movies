8beb8e978f4b687a9533b25a23baa252f78818f0
Toufik El bouamri <elb.toufik@gmail.com>
2022-03-28 12:12:55 +0200

    Added a movie detail rendering this.props.movie elements

--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -3,16 +3,18 @@ import React, { Component } from "react";
 export default class MovieElement extends Component {
   render() {
     return (
-      <div className="w-25 p-2">
-        <div className="card">
+      <div className="w-50 p-2">
+        <div className="border d-flex">
           <img
             src="https://m.media-amazon.com/images/I/51GA6V6VE1L._AC_SY445_.jpg"
-            className="card-img-top"
-            alt="a placeholder for a film"
+            alt="film"
+            width="150"
+            height="200"
           />
-          <div className="card-body">
-            <h5 className="card-title">Gladiator</h5>
-            <p className="card-text">lorem ipsum dolor sit amet, consectetur</p>
+          <div className="flex-fill d-flex flex-column p-3">
+            <h5>Titre</h5>
+            <hr className="w-100" />
+            <p>lorem ipsum dolor sit amet, consectetur</p>
           </div>
         </div>
       </div>
